3751e852ef1481e8b15a3b0135ac4421e82a5dd9
Sergey Korobin <cerkill@mail.ru>
2017-10-01 00:46:19 +0300

    adaptive ui + camera/library photo integration

--- TinkoffChat/ProfileViewController.swift
+++ TinkoffChat/ProfileViewController.swift
@@ -12,8 +12,8 @@ import AVFoundation
 
 class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    var calledfunction: String!
-    var imagePicker = UIImagePickerController()
+    var logString: String! // строка для сборки сообщения выбрасываемого в лог
+    var imagePicker = UIImagePickerController() // переменная для хранения информации и взаимодействия пользователя с встроенными функциями (использование камеры и галерреи) для использования этих данных программой
 
     @IBOutlet weak var nameLabel: UILabel!
     
@@ -23,40 +23,41 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
     
     @IBOutlet weak var profilePic: UIImageView!
     
+//    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+//        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+//        logString = "\(editButton.frame) - \(#function)"
+//        print(logString)
+//    }
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+    
+    //      SOLVE: Срабатывает ошибка. Связано это с тем, что на момент вызова инита, вью еще не успело прогрузиться и такого элемента как editButton еще попросту нет (= nil).
+    
     
     // Метод, вызывающийся  после того, как вьюшка контроллера была загружена в память.
     override func viewDidLoad() {
         super.viewDidLoad()
         imagePicker.delegate = self
+        logString = "\(editButton.frame) - \(#function)"
+        print(logString)
         editButton.layer.cornerRadius = 10
         editButton.layer.borderWidth = 1
         editButton.layer.borderColor = UIColor.black.cgColor
         addPhotoButton.layer.cornerRadius = 45
-        addPhotoButton.layer.backgroundColor = UIColor(hue: 0.6111, saturation: 0.73, brightness: 0.94, alpha: 1.0).cgColor // #3f78f0
+        addPhotoButton.layer.backgroundColor = UIColor(hue: 0.6111, saturation: 0.73, brightness: 0.94, alpha: 1.0).cgColor // разложенный цвет #3f78f0
         addPhotoButton.tintColor = UIColor.white
-        addPhotoButton.imageEdgeInsets = UIEdgeInsetsMake(20,20,20,20)
+        addPhotoButton.imageEdgeInsets = UIEdgeInsetsMake(20,20,20,20) // создание отступа картинки от границ
         profilePic.layer.cornerRadius = 45
         profilePic.clipsToBounds = true
-        
-        calledfunction = "Being called: " + FuncName() + "\n"
-        print(calledfunction)
     }
     
     @IBAction func editAction(_ sender: Any) {
-//        let button: UIButton = (sender  as? UIButton) ?? UIButton() - если нет, то сделай мне новую кнопку
-//        button.titleLabel?.text = "kek"
         
-//        if let button = sender as? UIButton {
-//
-//        }
-//        ------------------------------------------
-//        guard sender is UIButton else{
-//            return
-//        }
-//        ------------------------------------------
-//        button!.titleLabel!.text = "yo" // force inwrapping (можно в outlets) а так не стоит   
     }
     
+    //Вызов при нажатии на кнопку изменения аватара
     @IBAction func changeAvatarAction(_ sender: Any) {
         print("Выбери изображение профиля")
         let alertChooseVariantForPhotoMaking = UIAlertController(title: "Выберите способ", message: nil, preferredStyle: .actionSheet)
@@ -77,43 +78,39 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
     //Метод, вызывающийся перед появлением view на экране. Уже после viewDidLoad. Перед анимацией.
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        calledfunction = "Being called: " + FuncName() + "\n"
-        print(calledfunction)
+
     }
     
     //Метод, вызывающийся после срабатывания viewWillAppear(). После анимации.
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        calledfunction = "Being called: " + FuncName() + "\n"
-        print(calledfunction)
+        logString = "\(editButton.frame) - \(#function)"
+        print(logString)
+        // SOLVED: Значение фрейма отличается от предыдущего поскольку именно на данном этапе (а не в viewDidLoad) вью полностью прогружено (+ подгрузило всю иерархию элементов на экране) и появилось на экране. Именно это значение фрейма можно считать истинным в данном кейсе.
     }
     
     //Метод, который вызвается при изменении layout’а view.
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
-        calledfunction = "Being called: " + FuncName() + "\n"
-        print(calledfunction)
+
     }
     
     //Метод, который вызвается после расстановки элементов на layout'е. После viewWillLayoutSubviews().
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        calledfunction = "Being called: " + FuncName() + "\n"
-        print(calledfunction)
+
     }
     
     //Метод, вызывающийся перед исчезновении view на экране. Перед анимацией.
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        calledfunction = "Being called: " + FuncName() + "\n"
-        print(calledfunction)
+
     }
     
     //Метод, вызывающийся после срабатывания viewWillDisappear(). После анимации.
     override func viewDidDisappear(_ animated: Bool) {
         super.viewDidDisappear(animated)
-        calledfunction = "Being called: " + FuncName() + "\n"
-        print(calledfunction)
+
     }
     
     override func didReceiveMemoryWarning() {
@@ -121,20 +118,21 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         // Dispose of any resources that can be recreated.
 
     }
-    //Функция, возвращающая имя функции, из которой она была вызвана.
-    func FuncName(string: String = #function) -> String {
-        return string
-    }
-    
+
+    //Функция обработки вызова камеры для изменения аватара пользователя
     func openCamera(){
         let cameraMediaType = AVMediaType.video
-        let cameraAuthorizationStatus = AVCaptureDevice.authorizationStatus(for: cameraMediaType)
+        let cameraAuthorizationStatus = AVCaptureDevice.authorizationStatus(for: cameraMediaType) // статус доступа или отсутствии доступа к камере
+        
+        // алерт на отсутсвие камеры (создано для проверки в симуляторе)
         let warningNoCameraAlert  = UIAlertController(title: "Внимание", message: "Камера отсутствует!", preferredStyle: .alert)
         warningNoCameraAlert.addAction(UIAlertAction(title: "ОК, я в симуляторе relax", style: .default, handler: nil))
         
+        // алерт из - за пользовательского запрета доступа к камере
         let deniedCameraAccessCameraAlert  = UIAlertController(title: "Внимание", message: "Доступ к камере запрещен! Разрешите доступ к ней в настройках.", preferredStyle: .alert)
         deniedCameraAccessCameraAlert.addAction(UIAlertAction(title: "ОК", style: .default, handler: nil))
         
+        // логика по проверке статусов (не)/разрешения пользованием доступа к камере
         switch cameraAuthorizationStatus {
         case .denied:
             self.present(deniedCameraAccessCameraAlert, animated: true, completion: nil)
@@ -171,11 +169,14 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         }
     }
     
+    //Функция обработки вызова галереи для изменения аватара пользователя
     func openLibrary()
     {
-        let photosStatus = PHPhotoLibrary.authorizationStatus()
+        let photosStatus = PHPhotoLibrary.authorizationStatus() // статус доступа к галерее
+        // алерт из - за пользовательского запрета доступа к галерее
         let deniedGalleryAccessAlert  = UIAlertController(title: "Ошибка", message: "Вход в галерею не был разрешен. Разрешите доступ к фото в настройках.", preferredStyle: .alert)
         deniedGalleryAccessAlert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+        //логика взаимодействия со статусами доступа
         if photosStatus == .notDetermined {
             PHPhotoLibrary.requestAuthorization({status in
                 if status == .authorized{
@@ -201,7 +202,8 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         
     }
     
-    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+    // необхолимая функция для передачи данных о выбранном объекте - программе. Логика связывания выбранного изображения (редактированного или нет) к созданному в данной программе UIImageView profilePic.
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         if let imageEdited = info[UIImagePickerControllerEditedImage] as? UIImage
         {
             profilePic.image = imageEdited
